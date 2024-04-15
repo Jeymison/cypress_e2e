27a4ad1da541433eb7ac28a7ef7ca433f4bfc460
Jeymison Menezes <jeymisong@gmail.com>
2024-04-15 17:24:24 -0300

    Atualizando

--- cypress.config.js
+++ cypress.config.js
@@ -6,4 +6,12 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-});
+  reporter: 'mochawesome',
+  reporterOptions: {
+    reportDir: 'cypress/relatorio', // crie a pasta para alocar os relatorios
+    overwrite: true, // subescreve os arquivos para nao gerar novos
+    hmtl: true,
+    json: false, // para nao criar arquivos Json
+    timestamp: 'ddmmyyyy_HHMMss' // data
+  }
+}); 
--- cypress/e2e/ebac-jobs/candidatura.cy.js
+++ cypress/e2e/ebac-jobs/candidatura.cy.js
@@ -2,12 +2,13 @@
 
 describe('Testes para a pagina de candidatura', () => {
     beforeEach(() => {
-        cy.visit('https://ebac-jobs-e2e.vercel.app') // entra no site
+        cy.visit('https://ebac-jobs-e2e.vercel.app') 
     })
 
     it('Deve levar o usuario ate o formulario de inscricao', () => {
         cy.get('.Vaga_vagaLink__DeFkk').first().click()
-        cy.get('input').should('have.length', 7) // verifica quantos inputs tem na tela
+        cy.get('input').should('have.length', 7) 
+        cy.screenshot ('tela-inscricao')
     })
 
     it('Deve preencher o formulario de inscricao', () => {
@@ -16,12 +17,14 @@ describe('Testes para a pagina de candidatura', () => {
         cy.get('input[name="email"]').type('jeymison.cardoso@hotmail.com')
         cy.get('input[name="telefone"]').type('11 9999-9999')
         cy.get('input[name="endereco"]').type('Rua Alexandra Morais Campo')
-        cy.get('select[name="escolaridade"]').select('pos-graduado') // escolher a escolaridade
-        cy.get('#mac').check() // para escolher o servidor
+        cy.get('select[name="escolaridade"]').select('pos-graduado') 
+        cy.get('#mac').check() 
         cy.get('.Aplicacao_button__tw2AE').click()
 
         cy.on('window:alert', (conteudo) => {
             expect(conteudo).contain('Obrigado pela candidatura!')
-        }) // cria confirmacao de mensagem
+        }) 
+        
+        cy.screenshot ('tela-inscricao-preenchido')
     }) 
 })  
