b46309153fc71d132dcf7919052c660178e422a5
Jeymison Menezes <jeymisong@gmail.com>
2024-04-15 11:48:21 -0300

    Configurando o Cypress e criando o primeiro teste

---
+++ cypress/e2e/ebac-jobs/home.cy.js
@@ -0,0 +1,19 @@
+/// <reference types="cypress" />
+
+describe('Testes para home', () => {
+    it('Deve renderizar 4 vagas', () => {
+        cy.visit('https://ebac-jobs-e2e.vercel.app')
+        cy.get('.ListaVagas_vagas__gmNZn > li').should('have.length', 4)
+    })
+    
+    it('Deve filtar por fullstack', () => {
+        cy.visit('https://ebac-jobs-e2e.vercel.app') // entra no site
+        cy.get('.FormVagas_campo__E1ppF').type('fullstack') // procura o que deseja e filtra
+        cy.get('button[type="submit"]').click() // clica no botao para filtrar
+        cy.get('.ListaVagas_vagas__gmNZn > li').should('have.length', 1) // confirma que so tem um item da procura
+    })
+}) 
+
+// Utilizamos o > no get para retornar os filhos diretos 
+// cy.get('.FormVagas_campo__E1ppF').type('fullstack{enter}') Aqui digita e da enter no mesmo campo
+// cy.get('button[type="submit"]').click() aqui clica no botao para filtrar 
